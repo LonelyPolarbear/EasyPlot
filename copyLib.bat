f9dcad01bb80dd8fb319c32edace17b9e9c116ce
god1159360562 <god1159360562@163.com>
2025-10-03 15:01:07 +0800

    配置修改

---
+++ copyLib.bat
@@ -0,0 +1,7 @@
+copy .\3rdParty\freetype\*.dll .\output\bin\
+copy .\3rdParty\freetype\lib\*.lib .\output\bin\
+
+copy .\3rdParty\glew\bin\*.dll .\output\bin\
+copy .\3rdParty\glew\lib\*.lib .\output\bin\
+
+copy .\3rdParty\assimp\lib\*.lib .\output\bin\
