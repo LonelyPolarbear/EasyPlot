adf76fc9bc7f6006da9360aee05315730cf24340
LonelyPolarbear <god1159063562@163.com>
2026-02-11 16:24:17 +0800

    .cmake更新

---
+++ config_cmake/var.cmake
@@ -0,0 +1,16 @@
+set(SOURCES_FILES)																								#头文件路径
+set(HERDERS_FILES)																								#源文件路径
+set(LIB_INCLUDE_DIRECTORY)																				#包含目录
+set(LIB_NAMES)																										#lib名称
+set(LIB_DIRECTORIES)																							#lib包含目录
+set(ALL_SOURCES)																							#lib包含目录
+
+
+if(CMAKE_BUILD_TYPE  MATCHES "Debug")
+SET(DLL_DEBUG_POSTFIX "_d")  
+else() 
+SET(DLL_DEBUG_POSTFIX)
+endif()
+
+#
+list(APPEND LIB_NAMES MyLib::HeaderOnly)
