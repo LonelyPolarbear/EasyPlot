46da53917dff2110e4411c6e3cd28fef28fe700a
LonelyPolarbear <god1159063562@163.com>
2026-03-29 15:17:39 +0800

    删除xstorge

--- source/xstorage/XStorage.h
+++
@@ -1,9 +0,0 @@
-#include "XStorageApi.h"
-
-namespace XQ {
-	class lib_storage_API XStorage {
-	public:
-		XStorage();
-		void test();
-	};
-} // namespace XQ
--- source/xstorage/XStorageApi.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-#ifdef  LIB_STORAGE_DLL
-#define lib_storage_API __declspec(dllexport)
-#else
-#define lib_storage_API __declspec(dllimport)
-#endif 
