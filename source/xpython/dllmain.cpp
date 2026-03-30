5d59d54d2efbf2266c93a73cd96ed477fe3fb034
LonelyPolarbear <god1159063562@163.com>
2026-03-30 18:26:43 +0800

    增加sharevar项目管理全局配置

--- source/xpython/dllmain.cpp
+++ source/xpython/dllmain.cpp
@@ -2,19 +2,12 @@
 #include <windows.h>
 #include <iostream>
 #include <xlog/XLogger.h>
-#include "xpython/XPython.h"
 
-#include <pybind11/embed.h>
-void XPythonInit() {
-    //auto ins = XPython::Instance();
-	//ins->execute("import xjson");
-}
 
 BOOL APIENTRY DllMain(HMODULE hmod, DWORD reason, LPVOID reserved) {
     switch (reason) {
     case DLL_PROCESS_ATTACH:
         XLOG_TRACE("DLL xpython loaded");
-        XPythonInit();
         break;
     case DLL_PROCESS_DETACH:
         // cleanup if needed
